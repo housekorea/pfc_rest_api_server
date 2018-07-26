b14b073b699c5b53871f92fb0f27dc2aae0ba565
KgwangHee Han <house9737@gmail.com>
2018-07-26 16:27:48 +0900

    Add raw_data write file

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
 #!api_server/bin/python
-from flask import Flask,jsonify,abort,request,make_response,url_for
+from flask import Flask,jsonify,abort,request,make_response,url_for,request
+import json
+import sys
+from time import time
+
+
+DB_FILE_NAME = 'pfc_log_data.log'
+TOKEN_FILE_NAME = 'token_list.txt'
+
+def sensor_db_insert(authtoken,key,value,unix_ts):
+	print('this is error test ! ', file=sys.stderr)
+	with open(DB_FILE_NAME,"a") as logf:
+		log_raw = authtoken +',' + key + ',' + value  +','+unix_ts + "\r\n"
+		logf.write(log_raw)		
+
+
 
 app = Flask("PFC_REST_API_SERVER")
+
+query_string = {
+	'sensor' : [
+		'air_temp',
+		'air_hum',
+		'water_temp',
+		'ph',
+		'ec',
+		'ldr',
+		'co2',
+	],
+	'actuator' : {
+		'led',
+		'air_fan',
+		'ventil_fan',
+		'water_pump',
+		'ph_plus_pump',
+		'ph_minus_pump',
+		'sol_a_pump',
+		'sol_b_pump',
+		'ptc_heater',
+		'chiller',
+	}
+}
 
 
 @app.errorhandler(400)
@@ -21,12 +60,50 @@ def index():
 def v1_blynk_index():
 	return "Hello, this is blynk index"
 
-@app.route('/v1/<blynk_auth_token>/')
-def v1_blynk_auth_token():
-	return "Hello, Guyes"
+@app.route('/v1/<blynkauthtoken>/', methods=['GET','POST'])
+def v1_blynk_auth_token(blynkauthtoken):
+	with open(TOKEN_FILE_NAME) as f:
+		token_list = [line.rstrip() for line in f]
+
+
+	if request.method == 'GET':
+		unix_ts = str(int(time()))
+		for key in request.args:
+			if key in query_string['sensor']:
+				sensor_db_insert(blynkauthtoken,key,request.args[key],unix_ts)
+		return json.dumps(request.args)
+		# request.form['hum']
+		# request.form['temp']
+		# request.form['ph']
+		# request.form['ec']
+
+		return "this is GET request"
+	elif request.method == 'POST':
+		return "this is POST request"
 
 
 
+	if blynkauthtoken not in token_list:
+		return "Token is not exists on our list!" 
+
+
+
+	return "Exsits"
+
+
+
+
+	# def login():
+ #    error = None
+ #    if request.method == 'POST':
+ #        if valid_login(request.form['username'],
+ #                       request.form['password']):
+ #            return log_the_user_in(request.form['username'])
+ #        else:
+ #            error = 'Invalid username/password'
+ #    # the code below is executed if the request method
+ #    # was GET or the credentials were invalid
+ #    return render_template('login.html', error=error)
 
 
 
